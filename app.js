f3581d9da0637ef0896aabc57ae4a765ad8105bc
chad svastisalee <swasgrafx@gmail.com>
2021-06-23 15:46:02 -0500

    Connect to the database

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var config = require('./config.dev');
+var mongoose = require('mongoose');
 
 //Test the file
 // console.log(config);
 
 var app = express();
+
+//Connect to MongoDB
+mongoose.connect(config.mongodb, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
