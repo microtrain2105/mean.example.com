25487f0796465525e5fc5bb066a89970a7ba3647
chad svastisalee <swasgrafx@gmail.com>
2021-06-23 15:52:31 -0500

    Implement a route for api/users

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var config = require('./config.dev');
+var apiUsersRouter = require('./routes/api/users');
 var mongoose = require('mongoose');
 
 //Test the file
@@ -29,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/users', apiUsersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
